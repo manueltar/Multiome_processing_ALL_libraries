482d5a5ab446ae60b191aba6a3552784e640ade8
My Name <manueltar@hotmail.com>
2025-04-30 10:27:24 +0200

    corrected the problem in merge atac fragments

---
+++ SLURM_scripts/8_merge_atac_peaks_v2.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+#
+# =============================================================================
+# Job Script
+# =============================================================================
+
+
+#SBATCH --job-name=merge_frag
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=manuel.tardaguila@fht.org
+#SBATCH --partition=cpuq
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --output=merge_frag_%j.log
+#SBATCH --mem=96G
+
+# 16 96 --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+echo "Running on $(hostname)"
+echo "Started: $(date)"
+echo "========================"
+
+
+
+module load htslib-tools
+
+output_dir=$1
+
+cd "$output_dir"
+
+for SAMPLE in MCO_01326 MCO_01327 MCO_01328 MCO_01329 MCO_01330;do
+
+
+    echo "Processing: $SAMPLE $(date)" >&2
+   
+   if [[ "$SAMPLE" == 'MCO_01326' || "$SAMPLE" == 'MCO_01327' || "$SAMPLE" == 'MCO_01328' || "$SAMPLE" == 'MCO_01329' ]]; then
+
+       FILE=$(echo "/group/soranzo/manuel.tardaguila/2025_hESC_MK_multiome/""$SAMPLE""/outs/atac_fragments.tsv.gz")
+
+   else
+
+       FILE=$(echo "/group/soranzo/manuel.tardaguila/2025_hESC_lymph_multiome/Multiome/""$SAMPLE""/outs/atac_fragments.tsv.gz")       
+   fi
+
+   if [[ ! -f "$FILE" ]]; then
+        echo "WARNING: File not found for $SAMPLE at $FILE" >&2
+        continue
+    fi
+
+   echo "Using file: $FILE" >&2
+
+ zcat "$FILE" | awk -v SAMPLE="$SAMPLE"  'BEGIN{FS=OFS="\t"} {print $1,$2,$3,SAMPLE"_"$4,$5}'
+done | awk 'NF >= 5 && $2 ~ /^[0-9]+$/ && $3 ~ /^[0-9]+$/' | sort -k1,1 -k2,2n -S 72G | bgzip -c -@ 16 > $output_dir/processing_outputs/merged.atac_fragments.tsv.gz 
+
+tabix -p bed $output_dir/processing_outputs/merged.atac_fragments.tsv.gz
+
+
+
